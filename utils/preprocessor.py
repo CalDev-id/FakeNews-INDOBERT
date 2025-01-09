f0b7e187818726058768ee75091fbd43a9b6c47d
Heical Chandra <haical31@gmail.com>
2025-01-09 15:42:14 +0700

    Update preprocessor for mendaley test.py

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -329,11 +329,16 @@ class TwitterDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.recreate = recreate
         self.one_hot_label = one_hot_label
-        self.train_dataset_path = "datasets/train.csv"
-        self.validation_dataset_path = "datasets/validation.csv"
-        self.test_dataset_path = "datasets/test.csv"
-        self.processed_dataset_path = "datasets/twitter_label_manual_processed.csv"
-
+        # self.train_dataset_path = "datasets/train.csv"
+        # self.validation_dataset_path = "datasets/validation.csv"
+        # self.test_dataset_path = "datasets/test.csv"
+        # self.processed_dataset_path = "datasets/twitter_label_manual_processed.csv"
+            
+        #mendaley
+        self.train_dataset_path = "datasets/MendaleyTest/train.csv"
+        self.validation_dataset_path = "datasets/MendaleyTest/validation.csv"
+        self.test_dataset_path = "datasets/MendaleyTest/test.csv"
+        # self.processed_dataset_path = "datasets/twitter_label_manual_processed.csv"
     def load_data(self):
         # Load dataset if exists, else preprocess and save
         if os.path.exists(self.processed_dataset_path) and not self.recreate:
